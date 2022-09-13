ad1669031b7904182cdd9c78e74ec6510dff0b87
Michael <michaelbracey24@gmail.com>
2022-09-13 14:41:55 +0100

    candy changes

--- candy.js
+++ candy.js
@@ -1,18 +1,19 @@
 class Candy {
-    constructor(row, col, img, grid ) {
+    constructor(row, col, img, grid) {
         this.row = row
         this.col = col
+        // this.candyGridArray = candyGridArray
         this.texture = PIXI.Texture.from(`./img/${img}.png`)
         this.body = new PIXI.Sprite(this.texture)
-        this.body.position.x = 5 + (grid.width/9) * this.row 
-        this.body.position.y = 5 + ((grid.height/9) * this.col)
+        this.body.position.x = 5 + (grid.width/9) * this.col 
+        this.body.position.y = 5 + ((grid.height/9) * this.row)
         this.body.interactive = true
         grid.addChild(this.body)
 
-        this.body.on("pointerdown", this.dragStart)
+        this.body.on("pointerdown", this.dragStart.bind(this))
         this.body.on("pointerover", this.dragOver)
         // this.body.on("pointerupoutside", this.dragEnd.bind(this))
-        this.body.on("pointerup", this.dragEnd)
+        this.body.on("pointerup", this.dragEnd.bind(this))
 
     }
 
@@ -28,14 +29,19 @@ class Candy {
         }
     }
     dragEnd(e) {
-        otherCandy = this
-        console.log(otherCandy)
-        const otherCandyTexture = otherCandy.texture
-        const currCandyTexture = currCandy.texture
-        
-        otherCandy.texture = currCandyTexture
-        currCandy.texture = otherCandyTexture
 
+        otherCandy = this
+        console.log(otherCandy.body.position)
+        console.log(currCandy.body.position)
+
+        const otherCandyPos = otherCandy.body.position
+        const currCandyPos = currCandy.body.position
+        
+        otherCandy.body.position = currCandyPos
+        currCandy.body.position = otherCandyPos
+
+        // console.log(otherCandy.body.position)
+        // console.log(currCandy.body.position)
         this.alpha = 1;
         currCandy.alpha = 1;
         this.dragging = false;
