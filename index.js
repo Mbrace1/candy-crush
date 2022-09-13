c34a58bb29a356373e644cd8e5582349457c7d40
Michael <michaelbracey24@gmail.com>
2022-09-13 16:00:13 +0100

    only allowed to move to next candy

--- index.js
+++ index.js
@@ -37,8 +37,9 @@ for (let r = 0; r < rows; r++) {
         let texture = PIXI.Texture.from(`./img/${randomCandy()}.png`)
         let body = new PIXI.Sprite(texture)
         body.position.x = 5 + (candyGrid.width/9) * c
-        body.position.y = 5 + ((candyGrid.height/9) * r)
+        body.position.y = 5 + (candyGrid.height/9) * r
         body.interactive = true
+        body.accessibleTitle = `${r}-${c}`
         candyGrid.addChild(body)
 
         body.on("pointerdown", dragStart)
@@ -62,19 +63,29 @@ function dragOver(e) {
     }
 }
 function dragEnd(e) {
-
     otherCandy = this
-    console.log(otherCandy.texture)
-    console.log(currCandy)
-    swap(otherCandy, "texture", currCandy, "texture")
-    // const otherCandyPos = otherCandy.position
-    // const currCandyPos = currCandy.position
-    
-    // otherCandy.position = currCandyPos
-    // currCandy.position = otherCandyPos
+    let currCandyRowCol = currCandy.accessibleTitle.split('-')
+    let otherCandyRowCol = otherCandy.accessibleTitle.split('-')
+    let r1 = parseInt(currCandyRowCol[0])
+    let c1 = parseInt(currCandyRowCol[1])
+    let r2 = parseInt(otherCandyRowCol[0])
+    let c2 = parseInt(otherCandyRowCol[1])
 
-    // console.log(otherCandy.body.position)
-    // console.log(currCandy.body.position)
+    // only move by one adjacent
+    let moveLeft = c2 === c1 - 1 && r2 === r1
+    let moveRight = c2 === c1 + 1 && r2 === r1
+    let moveUp = c2 === c1 && r2 === r1 - 1
+    let moveDown = c2 === c1 && r2 === r1 + 1
+
+    let nextTo = moveLeft || moveRight || moveUp || moveDown
+
+    if(nextTo) {
+        swap(otherCandy, "texture", currCandy, "texture")
+    }
+
+    console.log(otherCandy)
+    console.log(currCandy)
+
     this.alpha = 1;
     currCandy.alpha = 1;
     this.dragging = false;
